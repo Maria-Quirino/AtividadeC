d8cd3b7fc6458af2bf4e3f4a46321fd9cfe48992
Maria-Quirino <Mariaqo29@outlook.com>
2020-10-20 09:45:46 -0300

    Programa calculadora em C

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+float soma;
+float menos;
+float mult;
+float divi;
+float n1;
+char op;
+float n2;
+
+int main()
+{ setlocale(LC_ALL,"Portuguese");
+    printf("Digite um número, a operação e outro número exemplo:(5+5)\n ");
+    scanf("%f %c %f",&n1,&op,&n2);
+    switch (op)
+    {
+        case '+':
+            soma= n1+n2;
+            printf("O resultado é:\n%.2f ", soma);
+        break;
+        case '-':
+            menos= n1-n2;
+            printf("O resultado é:\n%.2f ", menos);
+        break;
+        case 'x':
+            mult= n1*n2;
+            printf("O resultado é:\n%.2f ", mult);
+        break;
+        case '/':
+            if (n2>0)
+            {
+                divi= n1/n2;
+                printf("O resultado é:\n%.2f ",divi);
+            }
+            else
+            {
+                printf("Não existe divisão por zero!!!");
+            }
+        break;
+        default:
+        printf("Operação inválida");
+    }
+    return 0;
+}
